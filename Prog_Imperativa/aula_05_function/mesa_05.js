e84a4187b407b76945588c77bb9b3b1f204ce7ff
BrnBarreto <91027448+BrnBarreto@users.noreply.github.com>
2021-10-27 09:53:38 -0300

    Update mesa_05.js
    Termino das aividades

--- Prog_Imperativa/aula_05_function/mesa_05.js
+++ Prog_Imperativa/aula_05_function/mesa_05.js
@@ -1,5 +1,3 @@
-
-
 //Micro Desafio_1
 function teste1 (x,y) {
     return y - x;
@@ -43,4 +41,24 @@ function idadeCachorro(idade){
     let result = 7 * idade;
     return  result;
 }
-console.log(idadeCachorro(5));
+console.log("Os cachorro, possui "+idadeCachorro(5)+" anos");
+
+
+// Exercício 5 Crie uma função que calcule o valor da sua hora de trabalho, 
+// tendo como parâmetro o seu salário mensal.
+function valorHora(salario){
+    vHora = salario/160;
+    return vHora;
+}
+console.log("O valor da hora é: "+valorHora(7000))
+
+// Execício 6 - Crie a função calculadora de IMC* de uma pessoa,
+//que recebe a altura em centímetros e o peso em quilogramas. 
+//Em seguida, execute a função, testando diferentes valores. 
+//*IMC, significa Índice de Massa Corporal e é um parâmetro 
+//utilizado para saber se o peso está de acordo com a altura.
+function imc(Nome,altura, peso, ){
+    result = peso/(altura*altura);
+    return ("Senhor "+Nome+" seu IMC é de : "+ result)
+}
+console.log(imc("Bruno",1.66,80))

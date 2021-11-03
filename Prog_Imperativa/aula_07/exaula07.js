1a630891dcc16e84235a55a59592f6a2b866b4ac
BrnBarreto <91027448+BrnBarreto@users.noreply.github.com>
2021-11-03 20:20:55 -0300

    aula_07

---
+++ Prog_Imperativa/aula_07/exaula07.js
@@ -0,0 +1,35 @@
+// !true - // false
+// !false // true
+// !!false // true
+// !!true // true
+// !1 // false
+// !!1 // true
+// !0 // false
+// !!0 // true
+// !!"" // true
+
+
+//----------------------
+
+
+// let x = 5 ;
+// let y = 9;
+// console.log( (x < 10) && (x!==5));
+// console.log( (x>9) || (x===5));
+// console.log(!(x===y));
+
+// let x = 10;
+// let y = "a";
+// console.log((y==="b") || (x >= 10));
+
+// let x=3;
+// let y=8;
+// console.log(!((x == "3") || (x === y)) && (!y !== 8) && (x <= y));
+
+// let str = "";
+// let msg = "haha!";
+// let eBonito = "false";
+// console.log(!((str || msg) && eBonito));
+// //
+
+
---
+++ Prog_Imperativa/aula_07/operLogico.js
@@ -0,0 +1,53 @@
+/* ==========================
+ATRIBUIÇÃO
+========================== */
+
+// O operador "=" serve para atribuir o valor 40 a uma variável chamada "idade"
+
+let idade = 40
+
+/* ==========================
+ARITMÉTICOS
+========================== */
+
+// Nos permitem fazer operações matemáticas
+
+10 + 15 //---> Soma
+10 - 15 //---> Subtração
+10 * 15 //---> Multiplicação
+10 / 15 //---> Divisão
+15++    //---> Incremento de um em um: 15 + 1
+15--    //---> Decremento de um em um: 15 - 1
+15 % 5  //---> Módulo. O resto obtido da divisão de 15 por 5: 0
+15 % 2  //---> Módulo. O resto obtido da divisão de 15 por 2: 1
+
+/* ==========================
+COMPARAÇÃO SIMPLES
+========================== */
+
+10 == 15 // Igualdade false
+10 !== 15 // Desigualdade true
+
+/* ==========================
+COMPARAÇÃO ESTRITA
+========================== */
+10 === "10" // Igualdade strita false
+10 !== 15 // Desigualdade strita
+
+
+/* ==========================
+OPERADORES RELACIONAIS
+========================== */
+>
+>=
+<
+<=
+
+
+
+/* ==========================
+LÓGICOS
+========================== */
+&&  "e" - //Tudo verdadeiro
+! "not" 
+|| "OU"

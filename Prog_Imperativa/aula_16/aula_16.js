85a517ae218194dedd40682e5d7ecc49ab5419f3
Bruno Barreto <91027448+BrnBarreto@users.noreply.github.com>
2021-11-29 22:56:12 -0300

    agora vai

---
+++ Prog_Imperativa/aula_16/aula_16.js
@@ -0,0 +1,102 @@
+// let bemVindo = () =>"olá Mundo";
+
+// let dobro = numero => numero *2;
+
+// let soma = (a,b) => a+b;
+
+// let horaAtual= () => {
+//     let data = new Date();
+//     return data.getHours() + ":"+ data.getMinutes();
+// }
+
+// console.log(soma(1,2));
+// console.log(horaAtual());
+// console.log(dobro(3));
+// console.log(bemVindo());
+
+// callback --- 
+// setTimeout( function(){
+//     console.log("Ola Mundo")
+   
+// },1000);
+
+// setInterval(function (){
+//     console.log("Opa")
+// }, 1000)
+
+// let print = () =>{
+//     console.log("Ola Mundo");
+// }
+// setInterval(print,);;
+
+
+// function nomeCompleto(nome, sobrenome) {
+//     return nome+' '+sobrenome
+// }
+
+// console.log( nomeCompleto("Marcos", "Santana") );
+
+// function bomDia(nome, sobrenome, callback) {
+//     return 'Olá '+callback(nome, sobrenome)
+// }
+
+// // console.log( bomDia("Johnny", "Pecego", nomeCompleto) )A função deve atender aos seguintes requisitos:
+
+// ⦁	Você deve imprimir os números de 1 a 100;
+// ⦁	Se o número a ser impresso for um múltiplo de seu PRIMEIRO parâmetro, você deve imprimir a palavra “Fizz” em vez do número correspondente;
+// ⦁	Se o número a ser impresso for um múltiplo do seu SEGUNDO parâmetro, você deve imprimir a palavra "Buzz" em vez do número correspondente;
+// ⦁	Se o número for um múltiplo de AMBOS os parâmetros, você deve imprimir a palavra “FizzBuzz”.
+// Avançado: Como crédito extra, pense em como otimizar seu programa para que as palavras sejam passadas de forma dinâmica e seja você quem decide quais palavras ele deve dizer.
+
+
+
+let fizzBuss = (para1,para2) => {
+
+for ( let i = 0; 1<= 100; i++) {
+
+        if ( i % para1 == 0 && i % para2 == 0 ) {
+
+            console.log( "FizzBuzz" );
+
+        } else if (i % para1 == 0 ){
+
+            console.log("Fizz");
+
+        } else if (i % para2 == 0 ) {
+
+            console.log("Buzz");
+
+        } else {   
+
+            console.log(i);
+
+        }  
+
+    } 
+}
+
+fizzBuss(2,5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Prog_Imperativa/checkPoint2/formulario.js
@@ -0,0 +1,50 @@
+// let x = 5;
+// let y = x++ + ++x;
+// console.log("y= "+y);
+
+// x=3;
+// y=x*(x=1)*x++;
+// x=5;
+// y=3;
+// x=5;
+// y=3;
+// y*= x+1
+// console.log("x= "+x);
+// console.log(("y= "+y));
+
+// function soletrar (texto){
+
+//     console.log(texto.replace('-','').toLocaleUpperCase().split("").join("-"));
+// }
+// soletrar("digital-heouse");
+// soletrar("ctd");
+// let frutasAmarelas=["melão","Mamão","limão"];
+// let [fruta1,fruta2,fruta3]= frutasAmarelas;
+// console.log(fruta1);
+// console.log(fruta2);
+// console.log(fruta3);
+// console.log(frutasAmarelas);
+
+// let numeros= [10, 8, 7, 99, 0, 1, 6]
+// numeros.sort(function(a,b){ return b-a;});
+// numeros.sort(function(a,b){ return a*b;});
+// numeros.sort(function(a,b){ return b-a;});
+// numeros.sort(function(a,b){ return a,b;});
+// numeros.sort(function(a,b){ a-b;});
+
+// 
+
+// let frutasAmarelas=["Melão","mamão","limão"];
+// let frutasVermelhas=["a","b","c"];
+// let frutasVerdes=["h","g","f"];
+// frutasVermelhas1 = frutasVermelhas.map(fruta => fruta.toUpperCase());
+// console.log(frutasVermelhas);
+// let todasFrutas = [...frutasAmarelas,...frutasVermelhas1,...frutasVerdes];
+// console.log(todasFrutas);
+
+var z= 0;
+for (let index = 20; index < 50; index+=10) {
+    z+=index;
+}
+console.log(z)
+

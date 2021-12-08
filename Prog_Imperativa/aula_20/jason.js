74e6b0a2bacec8816e35dddff11a4fe603328502
Bruno Barreto <91027448+BrnBarreto@users.noreply.github.com>
2021-12-08 18:22:35 -0300

    mesas

---
+++ Prog_Imperativa/aula_20/jason.js
@@ -0,0 +1,61 @@
+const arquivos = [
+    {
+        
+        "nome": "ótimo",
+        "altura": 17
+    },
+    {
+        
+        "nome": "bom",
+        "altura": 13
+    },
+    {
+        
+        "nome": "regular",
+        "altura": 15
+    },
+    {
+        
+        
+        "nome": "",
+        "altura": 1.67
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Jacki Shurmer",
+        "altura": 1.75
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Jobi Mawtus",
+        "altura": 1.88
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Thomasin Latour",
+        "altura": 1.67
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Lonnie Verheijden",
+        "altura": 1.63
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Alonso Wannan",
+        "altura": 1.92
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Bendite Huggett",
+        "altura": 1.80
+    }
+  ]
+
+  module.exports = arquivos;

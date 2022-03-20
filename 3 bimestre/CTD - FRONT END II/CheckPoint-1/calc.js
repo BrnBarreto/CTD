8a494b0f13b6e3647bcd56750ee52d8c31ffefa5
Bruno Barreto <91027448+BrnBarreto@users.noreply.github.com>
2022-03-20 16:10:14 -0300

    check Point

---
+++ 3 bimestre/CTD - FRONT END II/CheckPoint-1/calc.js
@@ -0,0 +1,39 @@
+
+
+// Criando Formulário
+//    let criarDiv = document.createElement("div")
+//    document.body.appendChild(criarDiv);
+//    let criarForm = document.createElement("form");
+//    criarDiv.appendChild(criarForm);
+// criando um botão
+//    let abs = document.createElement("button")
+//    let abb = document.createTextNode("Ver mais")
+//    abs.appendChild(abb)
+//    document.body.appendChild(abs)
+
+
+
+
+   
+   function addImg () {
+
+      let linkimagem = document.querySelector(".abc").value;
+      let img = document.querySelector("#imagem"); 
+      img.setAttribute("src",linkimagem) 
+
+      let addtitle = document.querySelector("#titulo").value;
+      let title  = document.querySelector(".title"); 
+      title.innerHTML = addtitle;
+
+      let addDesc = document.querySelector("#descricao").value;
+      let desc  = document.querySelector("p");
+       desc.innerHTML = addDesc;
+      
+
+   } 
+
+   
+
+   
+   
+  

f89a4c610ed22a551190fed3fa559ac94b30bcd0
Bruno Barreto <91027448+BrnBarreto@users.noreply.github.com>
2022-03-21 05:22:44 -0300

    Atualização21/03/2022

--- 3 bimestre/CTD - FRONT END II/CheckPoint-1/calc.js
+++ 3 bimestre/CTD - FRONT END II/CheckPoint-1/calc.js
@@ -11,26 +11,42 @@
 //    abs.appendChild(abb)
 //    document.body.appendChild(abs)
 
+// function criaCard(){
+//    let linkimagem = document.querySelector(".abc").value;
+//    let addtitle = document.querySelector("#titulo").value;
+//    let addDesc = document.querySelector("#descricao").value;
 
 
+//    let card = 
+function{
+   
+let linkimagem = document.querySelector(".abc").value;
+let addtitle = document.querySelector("#titulo").value;
+let addDesc = document.querySelector("#descricao").value;
+
+
+
+}
 
    
-   function addImg () {
+   // function addImg () {
 
-      let linkimagem = document.querySelector(".abc").value;
-      let img = document.querySelector("#imagem"); 
-      img.setAttribute("src",linkimagem) 
+   //    let linkimagem = document.querySelector(".abc").value;
+   //    let img = document.querySelector("#imagem"); 
+   //    img.setAttribute("src",linkimagem) 
 
-      let addtitle = document.querySelector("#titulo").value;
-      let title  = document.querySelector(".title"); 
-      title.innerHTML = addtitle;
+   //    let addtitle = document.querySelector("#titulo").value;
+   //    let title  = document.querySelector(".title"); 
+   //    title.innerHTML = addtitle;
 
-      let addDesc = document.querySelector("#descricao").value;
-      let desc  = document.querySelector("p");
-       desc.innerHTML = addDesc;
+   //    let addDesc = document.querySelector("#descricao").value;
+   //    let desc  = document.querySelector("p");
+   //     desc.innerHTML = addDesc;
       
 
-   } 
+   // } 
+
+   
 
    
 

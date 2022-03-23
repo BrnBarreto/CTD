6cc7bc9e57af519cd810c1adb4318e0d30c00836
Bruno Barreto <91027448+BrnBarreto@users.noreply.github.com>
2022-03-22 21:14:56 -0300

    Atualização 22/03/22

--- 3 bimestre/CTD - FRONT END II/CheckPoint-1/calc.js
+++ 3 bimestre/CTD - FRONT END II/CheckPoint-1/calc.js
@@ -17,34 +17,60 @@
 //    let addDesc = document.querySelector("#descricao").value;
 
 
-//    let card = 
-function{
+
+// let base = 
+//    `<div class="card">
+
+//    <img  id="imagem" src="${linkimagem}" alt="">
+
+//    <h2 class="title"> Naruto Criança </h2>
+//    <br>
+//    <p id="paragrafo">Dolore quidem iste </p>
+
+// </div>  `;
+
+// let capturaCardpai = document.querySelector(".cardPai");
+// let criarDiv = document.createElement("div")
+// criarDiv.innerHTML = base;
+// capturaCardpai.appendChild(criarDiv)
+
+
+
+
+
+
    
-let linkimagem = document.querySelector(".abc").value;
-let addtitle = document.querySelector("#titulo").value;
-let addDesc = document.querySelector("#descricao").value;
+   function addImg () {
 
+      let linkimagem = document.querySelector(".abc").value;
+      let img = document.querySelector("#imagem"); 
+      img.setAttribute("src",linkimagem) 
 
+      // let addtitle = document.querySelector("#titulo").value;
+      // let title  = document.querySelector(".title"); 
+      // title.innerHTML = addtitle;
 
-}
-
-   
-   // function addImg () {
-
-   //    let linkimagem = document.querySelector(".abc").value;
-   //    let img = document.querySelector("#imagem"); 
-   //    img.setAttribute("src",linkimagem) 
-
-   //    let addtitle = document.querySelector("#titulo").value;
-   //    let title  = document.querySelector(".title"); 
-   //    title.innerHTML = addtitle;
-
-   //    let addDesc = document.querySelector("#descricao").value;
-   //    let desc  = document.querySelector("p");
-   //     desc.innerHTML = addDesc;
+      // let addDesc = document.querySelector("#descricao").value;
+      // let desc  = document.querySelector("p");
+      //  desc.innerHTML = addDesc;
       
-
-   // } 
+      let base = 
+      `<div class="card">
+   
+      <img  id="imagem" src="${linkimagem}" alt="">
+   
+      <h2 class="title"> Naruto Criança </h2>
+      <br>
+      <p id="paragrafo">Dolore quidem iste </p>
+   
+   </div>  `;
+   
+   let capturaCardpai = document.querySelector(".cardPai");
+   let criarDiv = document.createElement("div")
+   criarDiv.innerHTML = base;
+   capturaCardpai.appendChild(criarDiv)
+   
+   }
 
    
 
